b38b6a3bcfc1967726f7cfa96593bcc2166c465b
steven <stevennguyen291203@gmail.com>
2024-05-14 11:27:09 +0700

    alomos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const collection = require("./config");
+const collection = require("./acc");
 const bcrypt = require('bcrypt');
 
 const app = express();
