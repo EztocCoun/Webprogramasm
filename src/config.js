12d020dfeccf3495ea83e825a994c402350b866a
Cao Tran Gia An <75743409+acmis1@users.noreply.github.com>
2024-05-16 06:32:03 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connect = mongoose.connect("mongodb://localhost:27017/stevendb");
+const connect = mongoose.connect("mongodb://localhost:27017/youandmedb");
 
 // Check database connected or not
 connect.then(() => {
@@ -7,21 +7,27 @@ connect.then(() => {
 })
 .catch(() => {
     console.log("Database cannot be Connected");
-})
+});
 
 // Create Schema
 const Loginschema = new mongoose.Schema({
-    name: {
-        type:String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    zipcode: { type: String, required: true },
+    country: { type: String, required: true },
+    accountType: { type: String, required: true },
+    profilePicture: { type: String, required: true },
+    businessName: { type: String },
+    storeName: { type: String },
+    storeCategory: { type: String }
 });
 
-// collection part
+// Collection part
 const collection = new mongoose.model("users", Loginschema);
 
 module.exports = collection;
