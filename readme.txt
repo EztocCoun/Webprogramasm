b0a0fb5c0f05be9784970ecabf7ab223e715f21e
steven29122004 <165149053+steven29122004@users.noreply.github.com>
2024-05-16 14:53:52 +0700

    16/5

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Requirement installation:
 1. Download and install MongoDBCompass ( you can custom your path directory by choosing the custom option, not "complete; we will go furthur in the next few steps)
 2. Download and install NodeJS on browser.
 3. Open your VSCode and open terminal to install these npm (node package management):
-  - npm install bcrypt ejs nodemon mongoose express
+  - npm install bcrypt ejs nodemon mongoose express multer express-session
 
 4. Run the web by: "npm start" in the command
 5. Now go on browser and write localhost:5000
--- src/acc.js
+++ src/acc.js
@@ -7,21 +7,27 @@ connect.then(() => {
 })
 .catch(() => {
     console.log("Database cannot be Connected");
-})
+});
 
 // Create Schema
 const Loginschema = new mongoose.Schema({
-    name: {
-        type:String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    zipcode: { type: String, required: true },
+    country: { type: String, required: true },
+    accountType: { type: String, required: true },
+    profilePicture: { type: String, required: true },
+    businessName: { type: String },
+    storeName: { type: String },
+    storeCategory: { type: String }
 });
 
-// collection part
+// Collection part
 const collection = new mongoose.model("users", Loginschema);
 
 module.exports = collection;
