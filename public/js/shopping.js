2dcac29d32323156e998229f6558909d2a9ba16f
steven <stevennguyen291203@gmail.com>
2024-05-17 23:03:28 +0700

    update abput us and several page

---
+++ public/js/shopping.js
@@ -0,0 +1,246 @@
+//  Rmit University Vietnam
+// Course: COSC2430 Web Programming
+// Semester: 2024A
+// Author: Nhan, Thinh, Duy, Khang, An
+// ID: s3975356,s3992133, s3986546, s3979209, s4012479  
+
+let products = [];
+let cart = [];
+
+//* selectors
+const selectors = {
+  products: document.querySelector(".products"),
+  cartBtn: document.querySelector(".cart-btn"),
+  cartQty: document.querySelector(".cart-qty"),
+  cartClose: document.querySelector(".cart-close"),
+  cart: document.querySelector(".cart"),
+  cartOverlay: document.querySelector(".cart-overlay"),
+  cartClear: document.querySelector(".cart-clear"),
+  cartBody: document.querySelector(".cart-body"),
+  cartTotal: document.querySelector(".cart-total"),
+  searchForm: document.querySelector("#search-form"),
+  searchInput: document.querySelector("#search-query")
+};
+
+//* event listeners
+const setupListeners = () => {
+  document.addEventListener("DOMContentLoaded", initStore);
+
+  // product event
+  selectors.products.addEventListener("click", addToCart);
+
+  // cart events
+  selectors.cartBtn.addEventListener("click", showCart);
+  selectors.cartOverlay.addEventListener("click", hideCart);
+  selectors.cartClose.addEventListener("click", hideCart);
+  selectors.cartBody.addEventListener("click", updateCart);
+  selectors.cartClear.addEventListener("click", clearCart);
+
+  // search event
+  selectors.searchForm.addEventListener("submit", handleSearch);
+};
+
+//* event handlers
+const initStore = () => {
+  loadCart();
+  loadProducts("https://fakestoreapi.com/products")
+    .then(() => renderProducts(products))
+    .finally(renderCart);
+};
+
+const handleSearch = (e) => {
+  e.preventDefault(); // Prevent default form submission
+  const query = selectors.searchInput.value.trim().toLowerCase();
+  const filteredProducts = products.filter(product => 
+    product.title.toLowerCase().includes(query)
+  );
+  renderProducts(filteredProducts);
+};
+
+const showCart = () => {
+  selectors.cart.classList.add("show");
+  selectors.cartOverlay.classList.add("show");
+};
+
+const hideCart = () => {
+  selectors.cart.classList.remove("show");
+  selectors.cartOverlay.classList.remove("show");
+};
+
+const clearCart = () => {
+  cart = [];
+  saveCart();
+  renderCart();
+  renderProducts(products);
+  setTimeout(hideCart, 500);
+};
+
+const addToCart = (e) => {
+  if (e.target.hasAttribute("data-id")) {
+    const id = parseInt(e.target.dataset.id);
+    const inCart = cart.find((x) => x.id === id);
+
+    if (inCart) {
+      alert("Item is already in cart.");
+      return;
+    }
+
+    cart.push({ id, qty: 1 });
+    saveCart();
+    renderProducts(products);
+    renderCart();
+    showCart();
+  }
+};
+
+const removeFromCart = (id) => {
+  cart = cart.filter((x) => x.id !== id);
+
+  cart.length === 0 && setTimeout(hideCart, 500);
+
+  renderProducts(products);
+};
+
+const increaseQty = (id) => {
+  const item = cart.find((x) => x.id === id);
+  if (!item) return;
+
+  item.qty++;
+};
+
+const decreaseQty = (id) => {
+  const item = cart.find((x) => x.id === id);
+  if (!item) return;
+
+  item.qty--;
+
+  if (item.qty === 0) removeFromCart(id);
+};
+
+const updateCart = (e) => {
+  if (e.target.hasAttribute("data-btn")) {
+    const cartItem = e.target.closest(".cart-item");
+    const id = parseInt(cartItem.dataset.id);
+    const btn = e.target.dataset.btn;
+
+    btn === "incr" && increaseQty(id);
+    btn === "decr" && decreaseQty(id);
+
+    saveCart();
+    renderCart();
+  }
+};
+
+const saveCart = () => {
+  localStorage.setItem("online-store", JSON.stringify(cart));
+};
+
+const loadCart = () => {
+  cart = JSON.parse(localStorage.getItem("online-store")) || [];
+};
+
+//* render functions
+const renderCart = () => {
+  const cartQty = cart.reduce((sum, item) => {
+    return sum + item.qty;
+  }, 0);
+
+  selectors.cartQty.textContent = cartQty;
+  selectors.cartQty.classList.toggle("visible", cartQty);
+
+  selectors.cartTotal.textContent = calculateTotal().format();
+
+  if (cart.length === 0) {
+    selectors.cartBody.innerHTML =
+      '<div class="cart-empty">Your cart is empty.</div>';
+    return;
+  }
+
+  selectors.cartBody.innerHTML = cart
+    .map(({ id, qty }) => {
+      const product = products.find((x) => x.id === id);
+
+      const { title, image, price } = product;
+
+      const amount = price * qty;
+
+      return `
+        <div class="cart-item" data-id="${id}">
+          <img src="${image}" alt="${title}" />
+          <div class="cart-item-detail">
+            <h3>${title}</h3>
+            <h5>${price.format()}</h5>
+            <div class="cart-item-amount">
+              <i class="bi bi-dash-lg" data-btn="decr"></i>
+              <span class="qty">${qty}</span>
+              <i class="bi bi-plus-lg" data-btn="incr"></i>
+
+              <span class="cart-item-price">
+                ${amount.format()}
+              </span>
+            </div>
+          </div>
+        </div>`;
+    })
+    .join("");
+};
+
+const renderProducts = (productList) => {
+  selectors.products.innerHTML = productList
+    .map((product) => {
+      const { id, title, image, price } = product;
+
+      const inCart = cart.find((x) => x.id === id);
+
+      const disabled = inCart ? "disabled" : "";
+
+      const text = inCart ? "Added in Cart" : "Add to Cart";
+
+      return `
+    <div class="product">
+      <img src="${image}" alt="${title}" />
+      <h3>${title}</h3>
+      <h5>${price.format()}</h5>
+      <button ${disabled} data-id=${id}>${text}</button>
+    </div>
+    `;
+    })
+    .join("");
+};
+
+//* api functions
+const loadProducts = async (apiURL) => {
+  try {
+    const response = await fetch(apiURL);
+    if (!response.ok) {
+      throw new Error(`http error! status=${response.status}`);
+    }
+    products = await response.json();
+    console.log(products);
+  } catch (error) {
+    console.error("fetch error:", error);
+  }
+};
+
+//* helper functions
+const calculateTotal = () => {
+  return cart
+    .map(({ id, qty }) => {
+      const { price } = products.find((x) => x.id === id);
+
+      return qty * price;
+    })
+    .reduce((sum, number) => {
+      return sum + number;
+    }, 0);
+};
+
+Number.prototype.format = function () {
+  return this.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+};
+
+//* initialize
+setupListeners();
